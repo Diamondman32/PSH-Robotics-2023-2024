619cd6dbf1346cd67dc90e5a30efdc56708810f0
noltes8471 <noltes8471@parkwayschools.net>
2024-02-14 18:56:30 -0600

    Complete rewrite of auto. Looks much better

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/AutoCSRedLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/AutoCSRedLeft.java
@@ -26,208 +26,108 @@ public class AutoCSRedLeft extends LinearOpMode {
 
         robot.resetYaw();
 
+        //Dodging Pose Errors
         Trajectory error = robot.trajectoryBuilder(new Pose2d())
                 .forward(1)
                 .build();
-        Trajectory moveToCones = robot.trajectoryBuilder(error.end())
-                .lineToConstantHeading(new Vector2d(22,0))
-                .build();
-        Trajectory capMiddle = robot.trajectoryBuilder(moveToCones.end())
+
+        //Based on team prop position only one of these 3 will run
+        Trajectory capMiddle = robot.trajectoryBuilder(error.end())
                 .lineToConstantHeading(new Vector2d(31,0))
                 .build();
-        Trajectory backUp = robot.trajectoryBuilder(new Pose2d(moveToCones.end().getX(),moveToCones.end().getY(),-63))
-                .forward(-5)
+        Trajectory capLeft = robot.trajectoryBuilder(error.end())
+                .lineToLinearHeading(new Pose2d(20,5,Math.toRadians(35)))
                 .build();
-        Trajectory capLeft = robot.trajectoryBuilder(backUp.end())
-                .forward(6)
-                .build();
-        Trajectory capRight = robot.trajectoryBuilder(new Pose2d(moveToCones.end().getX(),moveToCones.end().getY(),63))
-                .lineToLinearHeading(new Pose2d(20,-5,Math.toRadians(-50)))
-                .build();
-        Trajectory unCapRight = robot.trajectoryBuilder(new Pose2d(capRight.end().getX(),capRight.end().getY(),-45))
-                .lineToLinearHeading(new Pose2d(22,0,Math.toRadians(0)))
+        Trajectory capRight = robot.trajectoryBuilder(error.end())
+                .lineToLinearHeading(new Pose2d(20,-5,Math.toRadians(35)))
                 .build();
 
-
-        Trajectory goToBoard1L = robot.trajectoryBuilder(new Pose2d(moveToCones.end().getX(),moveToCones.end().getY(),63))
+        //Move To go under truss
+        Trajectory goToBoard1FromLeft = robot.trajectoryBuilder(capLeft.end())
                 .lineToConstantHeading(new Vector2d(52,0))
                 .build();
-        Trajectory goToBoard2L = robot.trajectoryBuilder(new Pose2d(goToBoard1L.end().getX(),goToBoard1L.end().getY(),-90))
-                .lineToConstantHeading(new Vector2d(52,-60))
+        Trajectory goToBoard1FromMiddle = robot.trajectoryBuilder(capMiddle.end())
+                .lineToConstantHeading(new Vector2d(52,0))
                 .build();
-        Trajectory alignWithBoard1 = robot.trajectoryBuilder(goToBoard2L.end())
-                .lineToConstantHeading(new Vector2d(-5,-75))
-                .build();
-        Trajectory arriveAtBoard1 = robot.trajectoryBuilder(alignWithBoard1.end())
-                .forward(15)
+        Trajectory goToBoard1FromRight = robot.trajectoryBuilder(capRight.end())
+                .lineToConstantHeading(new Vector2d(52,0))
                 .build();
 
-        Trajectory goToBoard1M = robot.trajectoryBuilder(new Pose2d(capMiddle.end().getX(),capMiddle.end().getY(),45))
-                .forward(20)
-                .build();
-        Trajectory goToBoard2M = robot.trajectoryBuilder(new Pose2d(goToBoard1M.end().getX(),goToBoard1M.end().getY(),0))
-                .forward(17)
-                .build();
-        Trajectory goToBoard3M = robot.trajectoryBuilder(new Pose2d(52,0,-90))
-                .lineToConstantHeading(new Vector2d(52,-80))
-                .build();
-        Trajectory alignWithBoard2 = robot.trajectoryBuilder(new Pose2d(52,-80,-179))
-                .forward(25)
-                .build();
-        Trajectory arriveAtBoard2 = robot.trajectoryBuilder(alignWithBoard2.end())
-                .forward(28)
+        //Move Towards Board
+        Trajectory goToBoard2 = robot.trajectoryBuilder(new Pose2d(52,0,0))
+                .lineToLinearHeading(new Pose2d(52,-60,Math.toRadians(-135)))
                 .build();
 
-        Trajectory goToBoard1R = robot.trajectoryBuilder(new Pose2d(moveToCones.end().getX(),moveToCones.end().getY(),63))
-                .lineToConstantHeading(new Vector2d(54,0))
+        //Based on location of team prop, move to 1 of the 3 correct QR codes
+        // (NOTE: MIGHT NOT USE ROADRUNNER FOR THIS)
+        Trajectory goToLeftBoard = robot.trajectoryBuilder(goToBoard2.end())
+                .lineToLinearHeading(new Pose2d(-5,-75,Math.toRadians(-90)))//strafe
                 .build();
-        Trajectory goToBoard2R = robot.trajectoryBuilder(new Pose2d(goToBoard1R.end().getX(),goToBoard1R.end().getY(),-90))
-                .lineToConstantHeading(new Vector2d(54,-70))
+        Trajectory goToMiddleBoard = robot.trajectoryBuilder(goToBoard2.end())
+                .lineToLinearHeading(new Pose2d(-5,-75,Math.toRadians(-90)))//strafe
                 .build();
-        Trajectory alignWithBoard3 = robot.trajectoryBuilder(new Pose2d(goToBoard2R.end().getX(),goToBoard2R.end().getY(),-179))
-                .forward(29)
+        Trajectory goToRightBoard = robot.trajectoryBuilder(goToBoard2.end())
+                .lineToLinearHeading(new Pose2d(-5,-75,Math.toRadians(-90)))//strafe
                 .build();
-        Trajectory arriveAtBoard3 = robot.trajectoryBuilder(alignWithBoard3.end())
-                .forward(20)
-                .build();
+
+        robot.setPoseEstimate(new Pose2d());
 
         waitForStart();
         if (isStopRequested()) return;
-
-        telemetry.addData("yaw",robot.getYaw());
-        telemetry.update();
-
-        //Set Pose Estimate
-        robot.setPoseEstimate(new Pose2d());
-
-        //Find hanging Distance Value
-        ArrayList<Double> distance = new ArrayList<Double>();
-        double averageDist = 0;
-        timer.reset();
-        while (timer.seconds() < 1) {
-            distance.add(robot.getDist());
-        }
-        for (double dist : distance) {
-            averageDist+= dist;
-        }
-        averageDist /= distance.size();
+//----------------------------------------START OF AUTO---------------------------------------------
 
         //Tighten Grabbers
         robot.setLeftGrabber(0.37);
         robot.setRightGrabber(1.00);
 
-        //Drive to scan
-        robot.followTrajectory(moveToCones);
-        //scan forward if object cap it
-        if (robot.isDistClose(10)) {
-            telemetry.addData("dist",robot.getDist());
-            telemetry.update();
-            objFound = true;
-            objPos = 2;
-            robot.setPivot(0.1);//lower pixarm
-            sleep(500);
-            robot.followTrajectory(capMiddle);//push team prop
-            sleep(100);
-            robot.setLeftGrabber(0.61);//drop pixel
-            sleep(150);
-            robot.setPivot(0.6);//raise pixarm
-            sleep(50);
-            robot.setLeftGrabber(0.37);
-        }
-
-        if (!objFound) {
-            robot.turnDegrees(38);
-            telemetry.addData("dist",robot.getDist());
-            telemetry.update();
-            if (robot.isDistClose(10)) {
-                robot.followTrajectory(backUp);
-                objFound = true;
+        //Based on team prop position, cap a pixel on the correct tick mark
+        switch(robot.getObjectPosition()) {//Note: getObjectPosition doesn't work
+            case 1:
                 objPos = 1;
-                robot.setPivot(0.1);
+                robot.setPivot(0.1);//lower pix-arm
                 sleep(500);
-                robot.followTrajectory(capLeft);
-                robot.setLeftGrabber(0.61);
+                robot.followTrajectory(capLeft);//move over tick mark
                 sleep(100);
-                robot.setPivot(0.6);
+                robot.setLeftGrabber(0.61);//open claw (drop pixel)
+                sleep(150);
+                robot.setPivot(0.6);//raise pix-arm
                 sleep(50);
-                robot.setLeftGrabber(0.37);
+                robot.setLeftGrabber(0.37);//close claw
+                robot.followTrajectory(goToBoard1FromLeft);
+                robot.followTrajectory(goToBoard2);
+                robot.moveToAprilTag(4);
+                break;
+            case 2:
+                objPos = 2;
+                robot.setPivot(0.1);//lower pix-arm
                 sleep(500);
-            }
+                robot.followTrajectory(capMiddle);//move over tick mark
+                sleep(100);
+                robot.setLeftGrabber(0.61);//open claw (drop pixel)
+                sleep(150);
+                robot.setPivot(0.6);//raise pix-arm
+                sleep(50);
+                robot.setLeftGrabber(0.37);//close claw
+                robot.followTrajectory(goToBoard1FromMiddle);
+                robot.followTrajectory(goToBoard2);
+                robot.moveToAprilTag(5);
+                break;
+            case 3:
+                objPos = 3;
+                robot.setPivot(0.1);//lower pix-arm
+                sleep(500);
+                robot.followTrajectory(capRight);//move over tick mark
+                sleep(100);
+                robot.setLeftGrabber(0.61);//open claw (drop pixel)
+                sleep(150);
+                robot.setPivot(0.6);//raise pix-arm
+                sleep(50);
+                robot.setLeftGrabber(0.37);//close claw
+                robot.followTrajectory(goToBoard1FromRight);
+                robot.followTrajectory(goToBoard2);
+                robot.moveToAprilTag(6);
+                break;
         }
-
-        //if object found cap left otherwise cap right
-        if (!objFound) {
-            objPos = 3;
-            robot.setPivot(0.1);
-            sleep(100);
-            robot.followTrajectory(capRight);
-            robot.setLeftGrabber(0.61);
-            sleep(100);
-            robot.setPivot(0.6);
-            sleep(50);
-            robot.setLeftGrabber(0.37);
-            robot.followTrajectory(unCapRight);
-            robot.turnDegrees(0);
-            sleep(500);
-        }
-
-        if (objPos == 1) {
-            robot.turnDegrees(0);
-            robot.followTrajectory(goToBoard1L);
-            robot.setIsRotatedStartDeg(robot.getYaw());
-            robot.turnDegrees(-90);
-            robot.followTrajectory(goToBoard2L);
-            robot.turnDegrees(-90);
-            robot.followTrajectory(alignWithBoard1);
-            robot.setArmPos(1);
-            robot.setPivot(0.24);
-            sleep(500);
-            robot.followTrajectory(arriveAtBoard1);
-            robot.turnDegrees(-90,3);
-            robot.setRightGrabber(0.7);
-            sleep(100);
-            robot.setPivot(0.6);
-            robot.setArmPos(0);
-        }
-
-        if (objPos == 2) {
-            robot.turnDegrees(45);
-            robot.followTrajectory(goToBoard1M);
-            robot.turnDegrees(0);
-            robot.followTrajectory(goToBoard2M);
-            robot.turnDegrees(-90);
-            robot.followTrajectory(goToBoard3M);
-            robot.turnDegrees(-179);
-            robot.followTrajectory(alignWithBoard2);
-            robot.turnDegrees(-90);
-            robot.setArmPos(1);
-            robot.setPivot(0.27);
-            sleep(500);
-            robot.followTrajectory(arriveAtBoard2);
-            robot.setRightGrabber(0.7);
-            sleep(100);
-            robot.setPivot(0.6);
-            robot.setArmPos(0);
-        }
-        if (objPos == 3) {
-            robot.turnDegrees(0);
-            robot.followTrajectory(goToBoard1R);
-            robot.setIsRotatedStartDeg(robot.getYaw());
-            robot.turnDegrees(-90);
-            robot.followTrajectory(goToBoard2R);
-            robot.turnDegrees(-179);
-            robot.followTrajectory(alignWithBoard3);
-            robot.turnDegrees(-90);
-            robot.setArmPos(1);
-            robot.setPivot(0.24);
-            sleep(500);
-            robot.followTrajectory(arriveAtBoard3);
-            robot.setRightGrabber(0.7);
-            sleep(100);
-            robot.setPivot(0.6);
-            robot.setArmPos(0);
-        }
-
         sleep(3000);
     }
 }
