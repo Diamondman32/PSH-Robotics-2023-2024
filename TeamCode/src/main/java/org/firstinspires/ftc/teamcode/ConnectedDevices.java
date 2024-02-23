0a298208abcc61ac1886c18a2f474c4a8ad59c0f
Diamondman32 <samnolte0302@gmail.com>
2024-02-22 22:58:01 -0600

    Got rid of basically everything from previous commit and added it all back to new ConnectedDevices class. This is the format RR wants

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConnectedDevices.java
@@ -0,0 +1,248 @@
+package org.firstinspires.ftc.teamcode;
+
+import androidx.annotation.NonNull;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+public class ConnectedDevices {
+    private final Servo leftGrabber, rightGrabber, leftGrabberPivot, rightGrabberPivot;
+    private final DcMotorEx pixArm;
+    private final VisionPortal webcam;
+    private final TfodProcessor objectProcessor;
+    private final AprilTagProcessor QRProcessor;
+
+    public ConnectedDevices(HardwareMap hardwareMap) {
+        // pixArm Motor
+        pixArm = hardwareMap.get(DcMotorEx.class, "arm");
+        pixArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        pixArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        pixArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        pixArm.setTargetPosition(0);
+        pixArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // Servos
+        leftGrabber = hardwareMap.get(Servo.class, "grabLeft");
+        rightGrabber = hardwareMap.get(Servo.class, "grabRight");
+
+        leftGrabberPivot = hardwareMap.get(Servo.class, "grabPivotLeft");
+        rightGrabberPivot = hardwareMap.get(Servo.class, "grabPivotRight");
+
+        //   Image Processing
+        // QR Code Processor
+        QRProcessor = AprilTagProcessor.easyCreateWithDefaults();
+
+        // TensorFlow Processor
+        objectProcessor = new TfodProcessor.Builder()
+                .setMaxNumRecognitions(1)
+                .setUseObjectTracker(true)
+                .build();
+
+        // Webcam
+        webcam = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .addProcessor(QRProcessor)
+                .addProcessor(objectProcessor)
+                .enableLiveView(true)
+                .setAutoStopLiveView(false)
+                .build();
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+    private int level = 0;
+    private int armValue = 0;
+    public class SetArmPos implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            double power;
+            if (level == 0) {
+                armValue = 0;
+                power = 0.5;
+            } else if (level == 1) {
+                armValue = 75;
+                power = 0.7;
+            } else if (level == 2) {
+                armValue = 161;
+                power = 0.7;
+            } else {
+                power = 0;
+            }
+
+            pixArm.setTargetPosition(armValue);
+            pixArm.setPower(power);
+            return false;
+        }
+    }
+    public Action setArmPos(int level) {
+        this.level = level;
+        return new SetArmPos();
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+    private double pos;
+    private class SetPivot implements Action {
+        @Override
+        public boolean run(@NotNull TelemetryPacket telemetryPacket) {
+            rightGrabberPivot.setPosition(pos);
+            leftGrabberPivot.setPosition(1-pos);
+            return false;
+        }
+    }
+    public Action setPivot(double pos) {
+        this.pos = pos;
+        return new SetPivot();
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+    private boolean rightGrabberPos;
+    private class SetRightGrabber implements Action {
+        @Override
+        public boolean run(@NotNull TelemetryPacket telemetryPacket) {
+            if (rightGrabberPos)
+                rightGrabber.setPosition(.37); //Close
+            else
+                rightGrabber.setPosition(.61); //Open
+            return false;
+        }
+    }
+    public Action setRightGrabber(boolean rightGrabberPos) {
+        this.rightGrabberPos = rightGrabberPos;
+        return new SetRightGrabber();
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+    private boolean leftGrabberPos;
+    private class SetLeftGrabber implements Action {
+        @Override
+        public boolean run(@NotNull TelemetryPacket telemetryPacket) {
+            if (leftGrabberPos)
+                leftGrabber.setPosition(.37); //Close
+            else
+                leftGrabber.setPosition(.61); //Open
+            return false;
+        }
+    }
+    public Action setLeftGrabber(boolean leftGrabberPos) {
+        this.leftGrabberPos = leftGrabberPos;
+        return new SetLeftGrabber();
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+    // THIS MIGHT MESS UP ROADRUNNER
+    public void moveToAprilTag(int pos, MecanumDrive robot) {
+        final double DESIRED_DISTANCE = 12.0; //  this is how close the camera should get to the target (inches)
+        //  Set the GAIN constants to control the relationship between the measured position error, and how much power is
+        //  applied to the drive motors to correct the error.
+        //  Drive = Error * Gain    Make these values smaller for smoother control, or larger for a more aggressive response.
+        final double SPEED_GAIN  =  0.02  ;   //  Forward Speed Control "Gain". eg: Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
+        final double STRAFE_GAIN =  0.015 ;   //  Strafe Speed Control "Gain".  eg: Ramp up to 25% power at a 25 degree Yaw error.   (0.25 / 25.0)
+        final double TURN_GAIN   =  0.01  ;   //  Turn Control "Gain".  eg: Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
+
+        final double MAX_AUTO_SPEED = 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
+        final double MAX_AUTO_STRAFE= 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
+        final double MAX_AUTO_TURN  = 0.3;   //  Clip the turn speed to this max value (adjust for your robot)
+
+        final int DESIRED_TAG_ID = pos;
+
+        while (true) {
+            double x, y, yaw = 0;
+            AprilTagDetection desiredTag = null;
+
+            List<AprilTagDetection> currentDetections = QRProcessor.getDetections();
+            for (AprilTagDetection detection : currentDetections) {
+                // Look to see if we have size info on this tag.
+                if (detection.metadata != null) {
+                    //  Check to see if we want to track towards this tag.
+                    if ((DESIRED_TAG_ID < 0) || (detection.id == DESIRED_TAG_ID)) {
+                        // Yes, we want to use this tag.
+                        desiredTag = detection;
+                        break;  // don't look any further.
+                    } else {
+                        // This tag is in the library, but we do not want to track it right now.
+                        telemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
+                    }
+                } else {
+                    // This tag is NOT in the library, so we don't have enough information to track to it.
+                    telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
+                }
+            }
+
+            // Determine heading, range and Yaw (tag image rotation) error so we can use them to control the robot automatically.
+            double rangeError = (desiredTag.ftcPose.range - DESIRED_DISTANCE);
+            double headingError = desiredTag.ftcPose.bearing;
+            double yawError = desiredTag.ftcPose.yaw;
+
+            // Use the speed and turn "gains" to calculate how we want the robot to move.
+            x = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
+            y = Range.clip(-yawError * STRAFE_GAIN, -MAX_AUTO_STRAFE, MAX_AUTO_STRAFE);
+            yaw = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN);
+
+            double leftFrontPower    =  x -y -yaw;
+            double rightFrontPower   =  x +y +yaw;
+            double leftBackPower     =  x +y -yaw;
+            double rightBackPower    =  x -y +yaw;
+
+            // Normalize wheel powers to be less than 1.0
+            double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+            max = Math.max(max, Math.abs(leftBackPower));
+            max = Math.max(max, Math.abs(rightBackPower));
+
+            if (max > 1.0) {
+                leftFrontPower /= max;
+                rightFrontPower /= max;
+                leftBackPower /= max;
+                rightBackPower /= max;
+            }
+
+            // Send powers to the wheels.
+            robot.setMotorPower(leftFrontPower, rightFrontPower, leftBackPower, rightBackPower);
+
+            // Exit condition
+            if (desiredTag.ftcPose.range < DESIRED_DISTANCE) {
+                break;
+            }
+        }
+        robot.setMotorPower(0,0,0,0);
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+    public double getYaw(MecanumDrive robot) {
+        return robot.lazyImu.get().getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+    public int getObjectPosition() {
+        ArrayList<Integer> numList = new ArrayList<Integer>();
+        List<Recognition> recognitions = objectProcessor.getRecognitions();
+        for (Recognition detection : recognitions) {
+            if (detection.getConfidence() > 0.75 && detection.getLeft()<detection.getRight())
+                numList.add(1);
+            else if (detection.getConfidence() > 0.75 && detection.getLeft()>detection.getRight())
+                numList.add(2);
+            else
+                numList.add(3);
+        }
+        double total = 0;
+        for (int x : numList) {
+            total += x;
+        }
+        return (int) Range.clip(Math.round(total / numList.size()),1,3);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -207,130 +207,11 @@ public final class MecanumDrive {
             );
         }
     }
-    // -Personal-
-    private final Servo leftGrabber, rightGrabber, leftGrabberPivot, rightGrabberPivot;
-    private final DcMotor pixArm;
-    private final VisionPortal webcam;
-    private final TfodProcessor objectProcessor;
-    private final AprilTagProcessor QRProcessor;
-    public void setArmPos(int level) {
-        int armValue = 0;
-        double power = 0;
-        if (level == -1) {
-            power = 0;
-        } else if (level == 0) {
-            armValue = 0;
-            power = 0.5;
-        } else if (level == 1) {
-            armValue = 75;
-            power = 0.7;
-        } else if (level == 2) {
-            armValue = 161;
-            power = 0.7;
-        }
-
-        pixArm.setTargetPosition(armValue);
-        pixArm.setPower(power);
-    }
-    public void setPivot(double pos) {
-        rightGrabberPivot.setPosition(pos);
-        leftGrabberPivot.setPosition(1-pos);
-    }
-    public void setRightGrabber(double pos) {
-        rightGrabber.setPosition(pos);
-    }
-    public void setLeftGrabber(double pos) {
-        leftGrabber.setPosition(pos);
-    }
-    public double getYaw() {
-        return lazyImu.get().getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
-    }
-    public int getObjectPosition() {
-        List<Recognition> recognitions = objectProcessor.getRecognitions();
-        return 1;
-    }
-    public void manualMotorPower(double frontLeft, double frontRight, double backLeft, double backRight) { // THIS MIGHT MESS UP ROADRUNNER
-        leftFront.setPower(frontLeft);
-        leftBack.setPower(backLeft);
-        rightFront.setPower(frontRight);
-        rightBack.setPower(backRight);
-    }
-    public void moveToAprilTag(int pos) {
-        final double DESIRED_DISTANCE = 12.0; //  this is how close the camera should get to the target (inches)
-        //  Set the GAIN constants to control the relationship between the measured position error, and how much power is
-        //  applied to the drive motors to correct the error.
-        //  Drive = Error * Gain    Make these values smaller for smoother control, or larger for a more aggressive response.
-        final double SPEED_GAIN  =  0.02  ;   //  Forward Speed Control "Gain". eg: Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
-        final double STRAFE_GAIN =  0.015 ;   //  Strafe Speed Control "Gain".  eg: Ramp up to 25% power at a 25 degree Yaw error.   (0.25 / 25.0)
-        final double TURN_GAIN   =  0.01  ;   //  Turn Control "Gain".  eg: Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
-
-        final double MAX_AUTO_SPEED = 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
-        final double MAX_AUTO_STRAFE= 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
-        final double MAX_AUTO_TURN  = 0.3;   //  Clip the turn speed to this max value (adjust for your robot)
-
-        final int DESIRED_TAG_ID = pos;
-
-        while (true) {
-            boolean targetFound = false;
-            double x, y, yaw = 0;
-            AprilTagDetection desiredTag = null;
-
-            List<AprilTagDetection> currentDetections = QRProcessor.getDetections();
-            for (AprilTagDetection detection : currentDetections) {
-                // Look to see if we have size info on this tag.
-                if (detection.metadata != null) {
-                    //  Check to see if we want to track towards this tag.
-                    if ((DESIRED_TAG_ID < 0) || (detection.id == DESIRED_TAG_ID)) {
-                        // Yes, we want to use this tag.
-                        targetFound = true;
-                        desiredTag = detection;
-                        break;  // don't look any further.
-                    } else {
-                        // This tag is in the library, but we do not want to track it right now.
-                        telemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
-                    }
-                } else {
-                    // This tag is NOT in the library, so we don't have enough information to track to it.
-                    telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
-                }
-            }
-
-            // Determine heading, range and Yaw (tag image rotation) error so we can use them to control the robot automatically.
-            double rangeError = (desiredTag.ftcPose.range - DESIRED_DISTANCE);
-            double headingError = desiredTag.ftcPose.bearing;
-            double yawError = desiredTag.ftcPose.yaw;
-
-            // Use the speed and turn "gains" to calculate how we want the robot to move.
-            x = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
-            y = Range.clip(-yawError * STRAFE_GAIN, -MAX_AUTO_STRAFE, MAX_AUTO_STRAFE);
-            yaw = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN);
-
-            double leftFrontPower    =  x -y -yaw;
-            double rightFrontPower   =  x +y +yaw;
-            double leftBackPower     =  x +y -yaw;
-            double rightBackPower    =  x -y +yaw;
-
-            // Normalize wheel powers to be less than 1.0
-            double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-            max = Math.max(max, Math.abs(leftBackPower));
-            max = Math.max(max, Math.abs(rightBackPower));
-
-            if (max > 1.0) {
-                leftFrontPower /= max;
-                rightFrontPower /= max;
-                leftBackPower /= max;
-                rightBackPower /= max;
-            }
-
-            // Send powers to the wheels.
-            manualMotorPower(leftFrontPower, rightFrontPower, leftBackPower, rightBackPower);
-
-            // Exit condition
-            if (desiredTag.ftcPose.range < DESIRED_DISTANCE) {
-                break;
-            }
-        }
-        manualMotorPower(0,0,0,0);
+    public void setMotorPower(double frontLeftPower, double frontRightPower, double backLeftPower, double backRightPower) {
+        leftFront.setPower(frontLeftPower);
+        rightFront.setPower(frontRightPower);
+        leftBack.setPower(backLeftPower);
+        rightBack.setPower(backRightPower);
     }
     public MecanumDrive(HardwareMap hardwareMap, Pose2d pose) {
         this.pose = pose;
@@ -363,42 +244,6 @@ public final class MecanumDrive {
         localizer = new DriveLocalizer();
 
         FlightRecorder.write("MECANUM_PARAMS", PARAMS);
-
-
-        // -PERSONAL-
-        // pixArm Motor
-        pixArm = hardwareMap.get(DcMotorEx.class, "arm");
-        pixArm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        pixArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        pixArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        pixArm.setTargetPosition(0);
-        pixArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        // Servos
-        leftGrabber = hardwareMap.get(Servo.class, "grabLeft");
-        rightGrabber = hardwareMap.get(Servo.class, "grabRight");
-
-        leftGrabberPivot = hardwareMap.get(Servo.class, "grabPivotLeft");
-        rightGrabberPivot = hardwareMap.get(Servo.class, "grabPivotRight");
-
-        //   Image Processing
-        // QR Code Processor
-        QRProcessor = AprilTagProcessor.easyCreateWithDefaults();
-
-        // TensorFlow Processor
-        objectProcessor = new TfodProcessor.Builder()
-                .setMaxNumRecognitions(1)
-                .setUseObjectTracker(true)
-                .build();
-
-        // Webcam
-        webcam = new VisionPortal.Builder()
-                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
-                .addProcessor(QRProcessor)
-                .addProcessor(objectProcessor)
-                .enableLiveView(true)
-                .setAutoStopLiveView(false)
-                .build();
     }
 
     public void setDrivePowers(PoseVelocity2d powers) {
